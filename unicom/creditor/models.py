77ba20aec8e11b72f2d5749b99a1d591b191c842
boris <ostrbor@gmail.com>
2017-12-13 20:14:16 +0300

    Update credit model, add tests

--- unicom/creditor/models.py
+++ unicom/creditor/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.utils.translation import gettext as _
+from django.core.exceptions import ValidationError
 
 
 class Creditor(models.Model):
@@ -6,15 +8,35 @@ class Creditor(models.Model):
 
 
 class Credit(models.Model):
+    CONSUMER = 'CONS'
+    MORTGAGE = 'MORT'
+    AUTOLOAN = 'AUTO'
+    BUSINESS = 'BUSI'
+
+    credit_type_choices = [
+        (CONSUMER, _('Потребительский')),
+        (MORTGAGE, _('Ипотека')),
+        (AUTOLOAN, _('Автокредит')),
+        (BUSINESS, _('КМСБ'))
+    ]
+
     created = models.DateField(auto_now_add=True)
     updated = models.DateField(auto_now=True)
     rotation_start = models.DateTimeField()
     rotation_end = models.DateTimeField()
     name = models.CharField(max_length=200)
-    # TODO: add choices
-    credit_type = models.CharField(max_length=100)
-    # TODO: add validation - min<max
+    # TODO: restrict credit_types with Foreign Key constraint
+    credit_type = models.CharField(choices=credit_type_choices, max_length=100)
     # TODO: maybe replace scores with PositiveIntegerField
     score_min = models.IntegerField()
     score_max = models.IntegerField()
     creditor = models.ForeignKey(Creditor, on_delete=models.CASCADE)
+
+    def clean(self):
+        if self.score_max < self.score_min:
+            raise ValidationError(_('Maxmum score is lesser than minimum credit score'))
+
+    def save(self, *args, **kwargs):
+        # TODO: model cleaning can be avoided with create. Add validation in forms.
+        self.clean()
+        super().save(*args, **kwargs)
--- unicom/creditor/tests.py
+++ unicom/creditor/tests.py
@@ -1,3 +1,47 @@
-from django.test import TestCase
+from datetime import datetime
 
-# Create your tests here.
+from django.test import TestCase
+from django.core.exceptions import ValidationError
+
+from .models import Credit, Creditor
+
+now = datetime.now()
+
+
+class TestModels(TestCase):
+    def setUp(self):
+        self.creditor = Creditor.objects.create(name='creditor')
+        Credit.objects.create(rotation_start=now,
+                              rotation_end=now,
+                              name='credit',
+                              credit_type='any',
+                              score_min=1,
+                              score_max=2,
+                              creditor=self.creditor)
+
+    def test_credit_can_be_saved_with_any_credit_type(self):
+        credit = Credit.objects.get(name='credit')
+        self.assertNotIn(credit.credit_type, credit.credit_type_choices)
+
+    def test_credit_raises_if_max_score_lt_min_score_with_save(self):
+        with self.assertRaises(ValidationError):
+            credit = Credit(rotation_start=now,
+                            rotation_end=now,
+                            name='credit',
+                            credit_type='any',
+                            score_min=2,
+                            score_max=1,
+                            creditor=self.creditor)
+            credit.save()
+
+    def test_credit_not_raises_if_max_score_lt_min_score_with_create(self):
+        try:
+            Credit.objects.create(rotation_start=now,
+                                  rotation_end=now,
+                                  name='credit',
+                                  credit_type='any',
+                                  score_min=1,
+                                  score_max=2,
+                                  creditor=self.creditor)
+        except ValidationError:
+            self.fail()
