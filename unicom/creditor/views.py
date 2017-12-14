b53e8fbf5f649f79e07a42c3860b492ea6c2341a
boris <ostrbor@gmail.com>
2017-12-14 20:19:32 +0300

    Add filters in rest api

--- unicom/creditor/views.py
+++ unicom/creditor/views.py
@@ -1,4 +1,6 @@
 from rest_framework import viewsets
+from django_filters import rest_framework as filters
+
 from partner.serializers import ApplicationForCreditorSerializer
 from partner.models import ApplicationForCreditor
 
@@ -6,3 +8,5 @@ from partner.models import ApplicationForCreditor
 class ApplicationReadOnlyView(viewsets.ReadOnlyModelViewSet):
     serializer_class = ApplicationForCreditorSerializer
     queryset = ApplicationForCreditor.objects.all()
+    filter_backends = (filters.DjangoFilterBackend,)
+    filter_fields = ('status', 'client')
--- unicom/partner/views.py
+++ unicom/partner/views.py
@@ -1,4 +1,6 @@
 from rest_framework import viewsets, mixins
+from django_filters import rest_framework as filters
+
 from .serializers import ClientSerializer, ApplicationForCreditorSerializer
 from .models import Client, ApplicationForCreditor
 
@@ -9,9 +11,12 @@ class ClientViewSet(mixins.CreateModelMixin,
                     viewsets.GenericViewSet):
     queryset = Client.objects.all()
     serializer_class = ClientSerializer
+    filter_backends = (filters.DjangoFilterBackend,)
+    filter_fields = ('name', 'surname')
 
 
 class ApplicationCreateView(mixins.CreateModelMixin,
                             viewsets.GenericViewSet):
-    queryset = ApplicationForCreditor.objects.all()
+    # TODO: why set queryset for Create only view if there is model serializer?
+    queryset = ApplicationForCreditor.objects.none()
     serializer_class = ApplicationForCreditorSerializer
