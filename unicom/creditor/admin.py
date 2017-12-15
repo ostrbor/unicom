8f193edcb921d2e98f107806c867100cb97f7c93
boris <ostrbor@gmail.com>
2017-12-15 21:12:17 +0300

    Improve links in admin

--- unicom/creditor/admin.py
+++ unicom/creditor/admin.py
@@ -5,7 +5,7 @@ from .models import Creditor, Credit
 
 @admin.register(Credit)
 class CreditAdmin(admin.ModelAdmin):
-    list_display = ('created', 'updated', 'rotation_start', 'rotation_end',
+    list_display = ('id', 'created', 'updated', 'rotation_start', 'rotation_end',
                     'name', 'credit_type', 'score_min', 'score_max', 'creditor')
     search_fields = ('name', 'credit_type', 'creditor')
     date_hierarchy = 'created'
--- unicom/partner/admin.py
+++ unicom/partner/admin.py
@@ -1,20 +1,37 @@
 from django.contrib import admin
+from django.urls import reverse
+from django.utils.safestring import mark_safe
 
 from .models import Client, ApplicationForCreditor
 
 
 @admin.register(Client)
 class ClientAdmin(admin.ModelAdmin):
-    list_display = ('created', 'updated','name', 'middle_name', 'surname',
+    list_display = ('id', 'created', 'updated', 'name_link', 'middle_name', 'surname_link',
                     'birth_date', 'phone', 'passport', 'credit_score')
     search_fields = ('name', 'middle_name', 'surname', 'phone', 'passport')
     date_hierarchy = 'created'
     list_filter = ('created', 'updated')
 
+    def name_link(self, obj):
+        return mark_safe('<a href="{}">{}</a>'.format(
+            reverse("admin:partner_client_change", args=(obj.pk,)),
+            obj.name
+        ))
+
+    def surname_link(self, obj):
+        return mark_safe('<a href="{}">{}</a>'.format(
+            reverse("admin:partner_client_change", args=(obj.pk,)),
+            obj.surname
+        ))
+
+    name_link.short_description = 'name'
+    surname_link.short_description = 'surname'
+
 
 @admin.register(ApplicationForCreditor)
 class ApplicationForCreditorAdmin(admin.ModelAdmin):
-    list_display = ('created', 'updated', 'client',
+    list_display = ('id', 'created', 'updated', 'client',
                     'requested_credit', 'status')
     search_fields = ('client', 'status', 'requested_credit')
     date_hierarchy = 'created'
